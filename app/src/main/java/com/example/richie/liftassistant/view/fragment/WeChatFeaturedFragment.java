58e64022be46eafb9be71a010586171e18e33018
liu243106179 <liu243106179@gmail.com>
2016-05-21 13:23:36 +0800

    Rejected commit: Default (2)

--- app/src/main/java/com/example/richie/liftassistant/view/fragment/WeChatFeaturedFragment.java
+++ app/src/main/java/com/example/richie/liftassistant/view/fragment/WeChatFeaturedFragment.java
@@ -7,15 +7,16 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
-
 import com.example.richie.liftassistant.R;
+import com.example.richie.liftassistant.bean.WeChatFeaturedResult;
+import com.example.richie.liftassistant.api.ApiImpl;
 
-import java.io.IOException;
 
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.RequestBody;
-import okhttp3.Response;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 /**
  * 这个界面展示微信精选文章列表
@@ -24,6 +25,10 @@ import okhttp3.Response;
 public class WeChatFeaturedFragment extends Fragment {
     public static final String ARG_PAGE = "ARG_PAGE";
     private int mPage;
+    private static final String BASE_URL = "http://route.showapi.com";
+    private static final String API_KEY = "d8bb354290fa4362828d71504162b231";
+    private static final String APP_ID = "19189";
+
 
     public static WeChatFeaturedFragment newInstance(int page) {
         Bundle args = new Bundle();
@@ -45,13 +50,42 @@ public class WeChatFeaturedFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_page, container, false);
         TextView textView = (TextView) view;
-        textView.setText("这是展示微信精选文字页面" );
-        String url = "http://route.showapi.com/181-1";
-//        initData(url);
+        textView.setText("这是展示微信精选文字页面");
+        query();
         return view;
     }
 
+    private void query() {
+        //1.创建Retrofit对象
+        Retrofit retrofit = new Retrofit.Builder()
+                .addConverterFactory(GsonConverterFactory.create())//添加 json 转换器
+                .baseUrl(BASE_URL)//主机地址
+                .build();
+
+        //2.创建访问API的请求
+        ApiImpl.GetWeChatChoice service = retrofit.create(ApiImpl.GetWeChatChoice.class);
+        Call<WeChatFeaturedResult> call = service.getResult(APP_ID, API_KEY, "10", "0", "", "1");
 
 
+        //3.发送请求
+        call.enqueue(new Callback<WeChatFeaturedResult>() {
+            @Override
+            public void onResponse(Call<WeChatFeaturedResult> call, Response<WeChatFeaturedResult> response) {
+                //4.处理结果
+                if (response.isSuccessful()) {
+                    WeChatFeaturedResult result = response.body();
+                    if (result != null) {
+                        WeChatFeaturedResult.ShowapiResBodyBean entity = result.getShowapi_res_body();
+                    }
+                }
+            }
+
+            @Override
+            public void onFailure(Call<WeChatFeaturedResult> call, Throwable t) {
+
+            }
+
+        });
+    }
 
 }
